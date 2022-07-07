a5c4072aefe8fa865a35f43388874d2456cdd9ad
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-07-07 19:36:48 -0300

    feat(src/controller): create cars abstract

---
+++ src/controllers/carsController.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import Controller, { RequestWithBody } from './controller';
+import CarService from '../Services/car.service';
+import { Car } from '../interfaces/CarInterface';
+
+class CarController extends Controller<Car> {
+  private $route: string;
+
+  constructor(
+    service = new CarService(),
+    route = '/cars',
+  ) {
+    super(service);
+    this.$route = route;
+  }
+
+  get route() { return this.$route; }
+
+  async create(
+    req: RequestWithBody<Car>,
+    res: Response<Car>,
+    next: NextFunction,
+  ): Promise<typeof res | undefined> {
+    const { body } = req;
+
+    try {
+      const car = await this.service.create(body) as Car;
+      return res.status(201).json(car);
+    } catch (err) {
+      next();
+    }
+  }
+
+  async readOne(
+    req: Request<{ id: string }>,
+    res: Response<Car>,
+    next: NextFunction,
+  ): Promise<typeof res | undefined> {
+    const { id } = req.params;
+    console.log(id);
+    try {
+      const car = await this.service.readOne(id) as Car;
+      return res.status(200).json(car);
+    } catch (error) {
+      console.log(error);
+
+      next(error);
+    }
+  }
+}
+
+export default CarController;
