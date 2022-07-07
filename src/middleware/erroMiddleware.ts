87449c879e661122b9b1ee04c3e6ee003429bc8a
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-07-07 20:29:10 -0300

    fix: next param

--- src/middleware/erroMiddleware.ts
+++ src/middleware/erroMiddleware.ts
@@ -1,19 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import CustomError from '../../helpers/customErrors';
 
 const error = (
   err: CustomError,
   req: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _next: NextFunction,
+  next: NextFunction,
 ) => {
   console.log(err);
 
   if (err.statusCode) {
     return res.status(err.statusCode).json({ error: err.message });
   }
-  res.status(500).json({ message: 'Erro interno' });
+  return res.status(500).json({ message: 'Erro interno' });
+  next();
 };
 
 export default error;
