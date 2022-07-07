7c2a07354f2d67b8be259ac00e95019a5de88a9c
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-07-07 19:37:17 -0300

    feat(src/route): create custom route

---
+++ src/routes/route.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import Controller from '../controllers/controller';
+import { IMiddlewareInterface } from '../interfaces/MiddlewareInterface';
+
+export default class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: Controller<T>,
+    middleware: IMiddlewareInterface,
+    route: string = controller.route,
+  ) {
+    this.router.get(route, (req, res, next) => controller.read(req, res, next));
+    this.router.get(
+      `${route}/:id`,
+
+      (req, res, next) => controller.readOne(req, res, next),
+    );
+    this.router.post(
+      route,
+      middleware.create,
+      (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+      ) => controller.create(req, res, next),
+    );
+  }
+}
